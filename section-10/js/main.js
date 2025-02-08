f8c63ffa90edf84bdfa2cbbd0a6cd05acc3ed91a
MD ABU HASAN <abuhasan43098@gmail.com>
2025-02-08 11:40:36 +0600

    section-10 with Problem Solving

--- section-10/js/main.js
+++ section-10/js/main.js
@@ -5,3 +5,24 @@ $('.product-list').masonry({
     // columnWidth: '.grid-sizer',
     percentPosition: true
   })
+
+  $(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        dots: true,
+        nav: false,
+        autoplay: false,
+        responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+            }
+    });
+});
